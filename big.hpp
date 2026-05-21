c3e3f07f08c682f01a4f158d099d5b77fb036551
Isabella Bosia <izaberina@gmail.com>
2026-05-21 15:02:07 +0100

    switch to comba's column major layout

--- big.hpp
+++ big.hpp
@@ -9,6 +9,7 @@ struct big {
     constexpr big& operator+=(const big& other) {
         u64 carry = 0;
 #if 1
+        #pragma clang loop unroll(full)
         for (auto i = 0; i < limbs; i++)
             words[i] = __builtin_addcl(words[i], other.words[i], carry, &carry);
 #else
@@ -21,22 +22,50 @@ struct big {
         return *this;
     }
 
-    constexpr big& operator*=(const big& other) {
-        big acc;
+    constexpr big operator*(const big& other) const {
+        big out;
+#if 0
         for (auto i = 0; i < limbs; i++) {
             u64 carry = 0;
             for (auto j = 0; i+j < limbs; j++) {
-                auto tmp = u128(words[i]) * other.words[j] + carry + acc.words[i+j];
-                acc.words[i+j] = tmp;
+                auto tmp = u128(words[i]) * other.words[j] + carry + out.words[i+j];
+                out.words[i+j] = tmp;
                 carry = tmp >> 64;
             }
         }
-        return *this = acc;
+        return out;
+#else
+        u64 carry0 = 0, carry1 = 0;
+        for (auto i = 0; i < limbs; i++) {
+            u64 lo = carry0, hi = carry1, top = 0;
+
+            for (auto j = 0; j <= i; j++) {
+                auto p = u128(words[j]) * other.words[i-j];
+                u64 p_lo = p, p_hi = p >> 64, c = 0;
+
+                lo = __builtin_addcl(lo, p_lo, 0, &c);
+                hi = __builtin_addcl(hi, p_hi, c, &c);
+                top += c;
+            }
+
+            out.words[i] = lo;
+            carry0 = hi;
+            carry1 = top;
+        }
+        return out;
+#endif
     }
 
     constexpr bool operator==(const big& other) const = default;
     constexpr bool operator!=(const big& other) const = default;
 
-    constexpr big operator+(const big& other) const { auto tmp = *this; tmp += other; return tmp; }
-    constexpr big operator*(const big& other) const { auto tmp = *this; tmp *= other; return tmp; }
+    constexpr big operator+(const big& other) const {
+        auto tmp = *this;
+        tmp += other;
+        return tmp;
+    }
+    constexpr big& operator*=(const big& other) {
+        *this = *this * other;
+        return *this;
+    }
 };
