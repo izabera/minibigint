4373e4cb4396ebc49f38909a8bdf832be17f5c05
Isabella Bosia <izaberina@gmail.com>
2026-05-22 21:23:30 +0100

    preallocate

--- benchmark/gmp.cpp
+++ benchmark/gmp.cpp
@@ -42,7 +42,7 @@ u64 gmp_mul_impl  (const config& conf, int limbs) {
 
 u64 gmp_bimom_impl(const config& conf, int limbs) {
     mpz_t value;
-    mpz_init(value);
+    mpz_init2(value, MAXLIMBS * 64); // XXX: or is it MAXLIMBS+1?
     u64 checksum = 0;
 
     auto [n, k] = conf.binom;
